95b60aa8cc3782a57dc739c622965b53c2a7fe22
Marcel Stör <marcelstoer@users.noreply.github.com>
2017-10-05 09:44:42 +0200

    Provide tool-specific welcome message
    If the user didn’t manually modify app/include/user_version.h (`git
    diff` would know) the tool provides its own. Thus, the NodeMCU welcome
    message will be tool-specific. With this firmware binaries become
    identifiable as having been built with this Docker container.
    
    Fixes #30

--- cmd.sh
+++ cmd.sh
@@ -7,16 +7,34 @@ set -e
 # - INTEGER_ONLY=1, if you want the integer firmware
 # - FLOAT_ONLY=1, if you want the floating point firmware
 
+export BUILD_DATE COMMIT_ID BRANCH SSL
+BUILD_DATE="$(date "+%Y-%m-%d %H:%M")"
+COMMIT_ID="$(git rev-parse HEAD)"
+BRANCH="$(git rev-parse --abbrev-ref HEAD | sed -r 's/[\/\\]+/_/g')"
+
+# figure out whether SSL is enabled in user_config.h
+if grep -Eq "^#define CLIENT_SSL_ENABLE" app/include/user_config.h; then
+    SSL="true"
+else
+    SSL="false"
+fi
+
+# check whether the user made changes to the version file, if so then assume she wants to use a custom version rather
+# than the one that would be set here -> we can't modify it
+if git diff --quiet app/include/user_version.h; then
+    CAN_MODIFY_VERSION=true
+else
+    CAN_MODIFY_VERSION=false
+fi
+
 # use the Git branch and the current time stamp to define image name if IMAGE_NAME not set
 if [ -z "$IMAGE_NAME" ]; then
-  BRANCH="$(git rev-parse --abbrev-ref HEAD | sed -r 's/[\/\\]+/_/g')"
-  TIME_STAMP="$(date +%Y%m%d-%H%M)"
-  IMAGE_NAME=${BRANCH}_${TIME_STAMP}
+  IMAGE_NAME=${BRANCH}_"$(date "+%Y%m%d-%H%M")"
 else
   true
 fi
 
-# unpack esp-open-sdk.tar.gz in a directory that is NOT the bound mount directory (i.e. inside the Docker image)
+# unpack esp-open-sdk.tar.gz into a directory that is NOT the bound mount directory (i.e. inside the Docker image)
 export HOME=/tmp/esp
 rm -rf ${HOME}
 mkdir ${HOME}
@@ -32,12 +50,20 @@ fi
 
 export PATH=$PATH:$PWD/esp-open-sdk/sdk:$PWD/esp-open-sdk/xtensa-lx106-elf/bin
 export CCACHE_DIR=/opt/nodemcu-firmware/.ccache
-export BUILD_DATE="\\\"$(date "+%Y-%m-%d %H:%M")\\\""
+
 cd /opt/nodemcu-firmware
+
+# modify user_version.h to provide more info in NodeMCU welcome message, doing this by passing
+# EXTRA_CCFLAGS="-DBUILD_DATE=... AND -DNODE_VERSION=..." to make turned into an escaping/expanding nightmare for which
+# I never found a good solution
+if [ "$CAN_MODIFY_VERSION" = true ]; then
+  sed -i 's/\(NodeMCU [^"]*\)/\1 built with Docker provided by frightanic.com\\n\\tbranch: '"$BRANCH"'\\n\\tcommit: '"$COMMIT_ID"'\\n\\tSSL: '"$SSL"'\\n/g' app/include/user_version.h
+  sed -i 's/"unspecified"/"created on '"$BUILD_DATE"'\\n"/g' app/include/user_version.h
+fi
 
 # make a float build if !only-integer
 if [ -z "$INTEGER_ONLY" ]; then
-  make WRAPCC=`which ccache` EXTRA_CCFLAGS="-DBUILD_DATE=\"$BUILD_DATE\"" clean all
+  make WRAPCC="$(which ccache)" clean all
   cd bin
   srec_cat -output nodemcu_float_"${IMAGE_NAME}".bin -binary 0x00000.bin -binary -fill 0xff 0x00000 0x10000 0x10000.bin -binary -offset 0x10000
   # copy and rename the mapfile to bin/
@@ -49,11 +75,17 @@ fi
 
 # make an integer build
 if [ -z "$FLOAT_ONLY" ]; then
-  make WRAPCC=`which ccache` EXTRA_CCFLAGS="-DLUA_NUMBER_INTEGRAL -DBUILD_DATE=\"$BUILD_DATE\"" clean all
+  make WRAPCC="$(which ccache)" EXTRA_CCFLAGS="-DLUA_NUMBER_INTEGRAL" clean all
   cd bin
   srec_cat -output nodemcu_integer_"${IMAGE_NAME}".bin -binary 0x00000.bin -binary -fill 0xff 0x00000 0x10000 0x10000.bin -binary -offset 0x10000
   # copy and rename the mapfile to bin/
   cp ../app/mapfile nodemcu_integer_"${IMAGE_NAME}".map
+  cd ../
 else
   true
+fi
+
+# revert the changes made to the version params
+if [ "$CAN_MODIFY_VERSION" = true ]; then
+  git checkout app/include/user_version.h
 fi
